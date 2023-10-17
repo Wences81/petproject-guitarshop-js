b31d0309b64e990c304ff150071b15a6153c2e41
Wences81 <zhylinpochta@gmail.com>
2023-10-17 19:15:19 +0200

    17/10/2023

---
+++ components/Shopping/Shopping.js
@@ -0,0 +1,31 @@
+class Shopping {
+
+    render() {
+        const productsStore = localStorageUtil.getProducts();
+        let htmlCatalog = '';
+
+        CATALOG.forEach(({ id, name, price }) => {
+            if (productsStore.indexOf(id) !== -1) {
+                htmlCatalog += `
+                <tr>
+                    <td class='shopping-element__name'>${name}</td>
+                    <td class='shopping-element__price'>⚡️ ${price.toLocaleString()} EUR</td>
+                </tr>
+                `;
+
+            }
+
+        });
+
+        const html = `
+        <div class='shopping-container'>
+             <table>
+                 ${htmlCatalog}
+             </table>
+        </div>
+        `;
+        ROOT_SHOPPING.innerHTML = html;
+    }
+}
+
+const shoppingPage = new Shopping();
