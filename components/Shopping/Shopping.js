2740b148bb6e45edf66bf7c150e3a97f6064034d
Wences81 <zhylinpochta@gmail.com>
2023-10-18 15:44:24 +0200

    18/10/2023

--- components/Shopping/Shopping.js
+++ components/Shopping/Shopping.js
@@ -1,17 +1,23 @@
 class Shopping {
 
+    handleClear() {
+        ROOT_SHOPPING.innerHTML = '';
+    }
+
     render() {
         const productsStore = localStorageUtil.getProducts();
         let htmlCatalog = '';
+        let sunCatalog = 0;
 
         CATALOG.forEach(({ id, name, price }) => {
             if (productsStore.indexOf(id) !== -1) {
                 htmlCatalog += `
                 <tr>
-                    <td class='shopping-element__name'>${name}</td>
-                    <td class='shopping-element__price'>⚡️ ${price.toLocaleString()} EUR</td>
+                    <td class='shopping-element__name'>⚡️ ${name}</td>
+                    <td class='shopping-element__price'> ${price.toLocaleString()} EUR</td>
                 </tr>
                 `;
+                sunCatalog += price;
 
             }
 
@@ -19,8 +25,13 @@ class Shopping {
 
         const html = `
         <div class='shopping-container'>
+            <div class='shopping__close' onclick='shoppingPage.handleClear()'></div>
              <table>
                  ${htmlCatalog}
+                 <tr>
+                    <td class='shopping-element__name'>💥 FINAL PRICE :</td>
+                    <td class='shopping-element__price'>${sunCatalog.toLocaleString()} EUR</td>
+                 </tr>
              </table>
         </div>
         `;
